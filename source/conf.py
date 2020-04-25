1c67336fa29ecd041095fb53c2c724058485cc72
James Webb <james@ultra-horizon.com>
2020-04-25 22:46:44 +0100

    Remove created using sphinx

--- source/conf.py
+++ source/conf.py
@@ -53,3 +53,6 @@ html_theme = 'default'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Don't show created using sphinx
+html_show_sphinx = False

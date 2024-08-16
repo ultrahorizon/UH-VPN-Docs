34b45fe414815f10d6a63498572d121c69393f44
James Webb <james@ultra-horizon.com>
2024-08-16 13:16:36 +0400

    Update theme to RTD

--- source/conf.py
+++ source/conf.py
@@ -18,7 +18,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'UH VPN'
-copyright = '2020, Ultra Horizon Ltd'
+copyright = '2024, Ultra Horizon Ltd'
 author = 'Ultra Horizon Ltd'
 
 # The full version, including alpha/beta/rc tags
@@ -47,7 +47,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
